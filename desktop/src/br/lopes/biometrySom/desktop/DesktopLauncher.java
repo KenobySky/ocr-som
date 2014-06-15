2f51fc9533c59f047e173759c8218ea40256665d
Andr? Vin?cius Lopes <andrelopes1705@gmail.com>
2014-06-15 17:49:06 -0300

    initial commit

---
+++ desktop/src/br/lopes/biometrySom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.lopes.biometrySom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.lopes.biometrySom.BiometrySom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BiometrySom(), config);
+	}
+}
