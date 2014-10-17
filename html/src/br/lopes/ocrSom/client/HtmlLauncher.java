b8ecf91f2cfcfaf872e4059c21a35608a649faef
dermetfan <serverkorken@gmail.com>
2014-10-17 15:31:56 +0200

    fixed GWT definition and stuff

--- html/src/br/lopes/ocrSom/client/HtmlLauncher.java
+++ html/src/br/lopes/ocrSom/client/HtmlLauncher.java
@@ -1,9 +1,9 @@
 package br.lopes.ocrSom.client;
 
+import br.lopes.ocrSom.OcrSom;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import br.lopes.ocrSom.BiometrySom;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new BiometrySom();
+                return new OcrSom();
         }
 }
